e092a425500f9f7db0f68a5b4bfbc22d47d30ce8
Alexandru Dreptu <alexdreptu@gmail.com>
2020-05-28 17:02:53 +0300

    Refactor randmoAlphanumeric to support 3 different randomization styles: normal, mix and pure.
    Implement:
    - randomAlpha()
    - randomChoiceInt()
    - randomBIC()
    - randomBICInvalid()
    - randomBaseCurrencyInvalid()
    - randomBankIDCodeInvalid()
    - randomCustomerID()
    - randomAlternativeBankAccountNames()
    - randomBool()

--- validation_helper_test.go
+++ validation_helper_test.go
@@ -9,51 +9,107 @@ import (
 	. "github.com/alexdreptu/form3-accountapi-client"
 )
 
-// some default values
+const accountType = "accounts"
+
 const (
-	accountType = "accounts"
-	accountBIC  = "NWBKGB22"
+	alpha   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	numeric = "0123456789"
+)
+
+// alphanumeric styles
+const (
+	alphanumericStyleNormal = 0
+	alphanumericStyleMix    = 1
+	alphanumericStylePure   = 2
 )
 
 // for randomAlphanumeric
 const uppercase = true
 
-// for randomStringNumber
+// for randomNumberString
 const startWithZero = true
 
 // for randomBankIDItaly
 const accountNumberPresent = true
 
-var firstNamesMale = []string{
-	"Jacob", "Mason", "Ethan", "Noah",
-	"William", "Liam", "Jayden", "Michael",
-	"Alexander", "Aiden", "Daniel", "Matthew",
-	"Elijah", "James", "Anthony", "Benjamin",
-	"Joshua", "Andrew", "David", "Joseph",
-}
+var (
+	firstNamesMale = []string{
+		"Jacob",
+		"Mason",
+		"Ethan",
+		"Noah",
+		"William",
+		"Liam",
+		"Jayden",
+		"Michael",
+		"Alexander",
+		"Aiden",
+		"Daniel",
+		"Matthew",
+		"Elijah",
+		"James",
+		"Anthony",
+		"Benjamin",
+		"Joshua",
+		"Andrew",
+		"David",
+		"Joseph",
+	}
 
-var firstNamesFemale = []string{
-	"Sophia", "Emma", "Isabella", "Olivia",
-	"Ava", "Emily", "Abigail", "Mia",
-	"Madison", "Elizabeth", "Chloe", "Ella",
-	"Avery", "Addison", "Aubrey", "Lily",
-	"Natalie", "Sofia", "Charlotte", "Zoey",
-}
+	firstNamesFemale = []string{
+		"Sophia",
+		"Emma",
+		"Isabella",
+		"Olivia",
+		"Ava",
+		"Emily",
+		"Abigail",
+		"Mia",
+		"Madison",
+		"Elizabeth",
+		"Chloe",
+		"Ella",
+		"Avery",
+		"Addison",
+		"Aubrey",
+		"Lily",
+		"Natalie",
+		"Sofia",
+		"Charlotte",
+		"Zoey",
+	}
 
-var lastNames = []string{
-	"Smith", "Johnson", "Williams", "Jones",
-	"Brown", "Davis", "Miller", "Wilson",
-	"Moore", "Taylor", "Anderson", "Thomas",
-	"Jackson", "White", "Harris", "Martin",
-	"Thompson", "Garcia", "Martinez", "Robinson",
-}
+	lastNames = []string{
+		"Smith",
+		"Johnson",
+		"Williams",
+		"Jones",
+		"Brown",
+		"Davis",
+		"Miller",
+		"Wilson",
+		"Moore",
+		"Taylor",
+		"Anderson",
+		"Thomas",
+		"Jackson",
+		"White",
+		"Harris",
+		"Martin",
+		"Thompson",
+		"Garcia",
+		"Martinez",
+		"Robinson",
+	}
+)
 
 func randomFirstName() string {
 	length := len(firstNamesMale) + len(firstNamesFemale)
 	names := make([]string, length)
 	names = append(firstNamesMale, firstNamesFemale...)
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
-	return names[random.Intn(length)]
+	name := names[random.Intn(length)]
+	return name
 }
 
 func randomFullName() string {
@@ -67,12 +123,46 @@ func randomFullName() string {
 	return fullName
 }
 
-func randomAlphanumeric(length int, uppercase ...bool) string {
-	const alphanumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+func randomAlphanumeric(length, style int, uppercase ...bool) string {
 	chars := make([]byte, length)
-	for i := range chars {
-		chars[i] = alphanumeric[random.Intn(len(alphanumeric))]
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	styleNormal := func() {
+		const alphanumeric = alpha + numeric
+		for i := range chars {
+			chars[i] = alphanumeric[random.Intn(len(alphanumeric))]
+		}
+	}
+
+	switch style {
+	case alphanumericStyleNormal:
+		styleNormal()
+
+	case alphanumericStyleMix:
+		which := randomBool()
+		for i := range chars {
+			if which {
+				chars[i] = byte(alpha[random.Intn(len(alpha))])
+				which = randomBool()
+			} else {
+				chars[i] = byte(numeric[random.Intn(len(numeric))])
+				which = randomBool()
+			}
+		}
+
+	case alphanumericStylePure:
+		which := randomBool()
+		for i := range chars {
+			if which {
+				chars[i] = byte(alpha[random.Intn(len(alpha))])
+				which = !which
+			} else {
+				chars[i] = byte(numeric[random.Intn(len(numeric))])
+				which = !which
+			}
+		}
+
+	default:
+		styleNormal()
 	}
 
 	str := string(chars)
@@ -83,7 +173,22 @@ func randomAlphanumeric(length int, uppercase ...bool) string {
 	return str
 }
 
-func randomStringNumber(length int, startWithZero ...bool) string {
+func randomAlpha(length int, uppercase ...bool) string {
+	chars := make([]byte, length)
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := range chars {
+		chars[i] = alpha[random.Intn(len(alpha))]
+	}
+
+	str := string(chars)
+	if len(uppercase) != 0 && uppercase[0] {
+		str = strings.ToUpper(str)
+	}
+
+	return str
+}
+
+func randomNumberString(length int, startWithZero ...bool) string {
 	min := 1
 	max := 9
 	numbers := make([]string, length)
@@ -104,47 +209,152 @@ func randomLength(min, max int) int {
 	return rand.New(rand.NewSource(time.Now().UnixNano())).Intn(max+1-min) + min
 }
 
-func randomChoose(x, y int) int {
+func randomChoiceInt(x, y int) int {
 	numbers := []int{x, y}
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
-	return numbers[random.Intn(2)]
+	return numbers[random.Intn(len(numbers))]
 }
 
-// TODO: implement
 func randomBIC() string {
-	return ""
+	length := randomChoiceInt(BICLength8, BICLength11)
+	str := randomAlpha(6, uppercase)
+	if length == BICLength8 {
+		str += randomAlphanumeric(2, alphanumericStyleNormal, uppercase)
+	} else if length == BICLength11 {
+		str += randomAlphanumeric(5, alphanumericStyleNormal, uppercase)
+	}
+	return str
+}
+
+func randomBICInvalid() string {
+	length := randomChoiceInt(BICLength8, BICLength11)
+	return randomAlphanumeric(length, alphanumericStylePure, uppercase)
+}
+
+func randomBaseCurrencyInvalid() string {
+	currencies := []string{
+		CurrencyUnitedKingdom,
+		CurrencyAustralia,
+		CurrencyBelgium,
+		CurrencyCanada,
+		CurrencyFrance,
+		CurrencyGermany,
+		CurrencyGreecee,
+		CurrencyHongKong,
+		CurrencyItaly,
+		CurrencyLuxembourg,
+		CurrencyNetherlands,
+		CurrencyPoland,
+		CurrencyPortugal,
+		CurrencySpain,
+		CurrencySwitzerland,
+		CurrencyUnitedStates,
+	}
+
+	currency := func() string {
+		var currency string
+		for {
+			currency = randomAlpha(3, uppercase)
+			for _, c := range currencies {
+				if currency != c {
+					return currency
+				}
+			}
+		}
+	}()
+
+	return currency
+}
+
+func randomBankIDCodeInvalid() string {
+	codes := []string{
+		BankIDCodeUnitedKingdom,
+		BankIDCodeAustralia,
+		BankIDCodeBelgium,
+		BankIDCodeCanada,
+		BankIDCodeFrance,
+		BankIDCodeGermany,
+		BankIDCodeGreece,
+		BankIDCodeHongKong,
+		BankIDCodeItaly,
+		BankIDCodeLuxembourg,
+		BankIDCodeNetherlands,
+		BankIDCodePoland,
+		BankIDCodePortugal,
+		BankIDCodeSpain,
+		BankIDCodeSwitzerland,
+		BankIDCodeUnitedStates,
+	}
+
+	code := func() string {
+		var code string
+		for {
+			code = randomAlpha(randomLength(2, 5), uppercase)
+			for _, c := range codes {
+				if code != c {
+					return code
+				}
+			}
+		}
+	}()
+
+	return code
+}
+
+func randomCustomerID() string {
+	return randomAlphanumeric(randomLength(5, 15), alphanumericStyleNormal, uppercase)
+}
+
+func randomAlternativeBankAccountNames(length ...int) []string {
+	var mustLength int
+	if len(length) != 0 {
+		mustLength = length[0]
+	} else {
+		mustLength = randomLength(1, 3)
+	}
+	names := make([]string, mustLength)
+	for i := range names {
+		names[i] = randomFullName()
+	}
+	return names
+}
+
+func randomBool() bool {
+	booleans := []bool{true, false}
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return booleans[random.Intn(len(booleans))]
 }
 
 func randomBankIDUnitedKingdom() string {
-	return randomStringNumber(BankIDLengthUnitedKingdom)
+	return randomNumberString(BankIDLengthUnitedKingdom)
 }
 
 func randomBankIDAustralia() string {
-	return randomStringNumber(BankIDLengthAustralia)
+	return randomNumberString(BankIDLengthAustralia)
 }
 
 func randomBankIDBelgium() string {
-	return randomStringNumber(BankIDLengthBelgium)
+	return randomNumberString(BankIDLengthBelgium)
 }
 
 func randomBankIDCanada() string {
-	return randomStringNumber(BankIDLengthCanada)
+	return randomNumberString(BankIDLengthCanada, startWithZero)
 }
 
 func randomBankIDFrance() string {
-	return randomStringNumber(BankIDLengthFrance)
+	return randomNumberString(BankIDLengthFrance)
 }
 
 func randomBankIDGermany() string {
-	return randomStringNumber(BankIDLengthGermany)
+	return randomNumberString(BankIDLengthGermany)
 }
 
 func randomBankIDGreece() string {
-	return randomStringNumber(BankIDLengthGreece)
+	return randomNumberString(BankIDLengthGreece)
 }
 
 func randomBankIDHongKong() string {
-	return randomStringNumber(BankIDLengthHongKong)
+	return randomNumberString(BankIDLengthHongKong)
 }
 
 func randomBankIDItaly(accountNumberPresent ...bool) string {
@@ -154,12 +364,11 @@ func randomBankIDItaly(accountNumberPresent ...bool) string {
 	} else {
 		length = BankIDLengthItalyAccountNumberNotPresent
 	}
-
-	return randomStringNumber(length)
+	return randomNumberString(length)
 }
 
 func randomBankIDLuxembourg() string {
-	return randomStringNumber(BankIDLengthLuxembourg)
+	return randomNumberString(BankIDLengthLuxembourg)
 }
 
 // returns empty string because BankID for Netherlands must be blank
@@ -168,89 +377,101 @@ func randomBankIDNetherlands() string {
 }
 
 func randomBankIDPoland() string {
-	return randomStringNumber(BankIDLengthPoland)
+	return randomNumberString(BankIDLengthPoland)
 }
 
 func randomBankIDPortugal() string {
-	return randomStringNumber(BankIDLengthPortugal)
+	return randomNumberString(BankIDLengthPortugal)
 }
 
 func randomBankIDSpain() string {
-	return randomStringNumber(BankIDLengthSpain)
+	return randomNumberString(BankIDLengthSpain)
 }
 
 func randomBankIDSwitzerland() string {
-	return randomStringNumber(BankIDLengthSwitzerland)
+	return randomNumberString(BankIDLengthSwitzerland)
 }
 
 func randomBankIDUnitedStates() string {
-	return randomStringNumber(BankIDLengthUnitedStates)
+	return randomNumberString(BankIDLengthUnitedStates)
 }
 
 func randomAccountNumberUnitedKingdom() string {
-	return randomStringNumber(AccountNumberLengthUnitedKingdom, startWithZero)
+	return randomNumberString(AccountNumberLengthUnitedKingdom)
 }
 
 func randomAccountNumberAustralia() string {
-	length := randomLength(AccountNumberLengthAustraliaStart, AccountNumberLengthAustraliaStop)
-	return randomStringNumber(length)
+	length := randomLength(
+		AccountNumberLengthAustraliaStart,
+		AccountNumberLengthAustraliaStop,
+	)
+	return randomNumberString(length)
 }
 
 func randomAccountNumberBelgium() string {
-	return randomStringNumber(AccountNumberLengthBelgium)
+	return randomNumberString(AccountNumberLengthBelgium)
 }
 
 func randomAccountNumberCanada() string {
-	length := randomLength(AccountNumberLengthCanadaStart, AccountNumberLengthCanadaStop)
-	return randomStringNumber(length)
+	length := randomLength(
+		AccountNumberLengthCanadaStart,
+		AccountNumberLengthCanadaStop,
+	)
+	return randomNumberString(length)
 }
 
 func randomAccountNumberFrance() string {
-	return randomStringNumber(AccountNumberLengthFrance)
+	return randomNumberString(AccountNumberLengthFrance)
 }
 
 func randomAccountNumberGermany() string {
-	return randomStringNumber(AccountNumberLengthGermany)
+	return randomNumberString(AccountNumberLengthGermany)
 }
 
 func randomAccountNumberGreece() string {
-	return randomStringNumber(AccountNumberLengthGreece)
+	return randomNumberString(AccountNumberLengthGreece)
 }
 
 func randomAccountNumberHongKong() string {
-	length := randomLength(AccountNumberLengthHongKongStart, AccountNumberLengthHongKongStop)
-	return randomStringNumber(length)
+	length := randomLength(
+		AccountNumberLengthHongKongStart,
+		AccountNumberLengthHongKongStop,
+	)
+	return randomNumberString(length)
 }
 
 func randomAccountNumberItaly() string {
-	return randomStringNumber(AccountNumberLengthItaly)
+	return randomNumberString(AccountNumberLengthItaly)
 }
 
 func randomAccountNumberLuxembourg() string {
-	return randomStringNumber(AccountNumberLengthLuxembourg)
+	return randomNumberString(AccountNumberLengthLuxembourg)
 }
 
 func randomAccountNumberNetherlands() string {
-	return randomStringNumber(AccountNumberLengthNetherlands)
+	return randomNumberString(AccountNumberLengthNetherlands)
 }
 
 func randomAccountNumberPoland() string {
-	return randomStringNumber(AccountNumberLengthPoland)
+	return randomNumberString(AccountNumberLengthPoland)
 }
 
 func randomAccountNumberPortugal() string {
-	return randomStringNumber(AccountNumberLengthPortugal)
+	return randomNumberString(AccountNumberLengthPortugal)
 }
 
 func randomAccountNumberSpain() string {
-	return randomStringNumber(AccountNumberLengthSpain)
+	return randomNumberString(AccountNumberLengthSpain)
 }
 
 func randomAccountNumberSwitzerland() string {
-	return randomStringNumber(AccountNumberLengthSwitzerland)
+	return randomNumberString(AccountNumberLengthSwitzerland)
 }
 
 func randomAccountNumberUnitedStates() string {
-	length := randomLength(AccountNumberLengthUnitedStatesStart, AccountNumberLengthUnitedStatesStop)
-	return randomStringNumber(length)
+	length := randomLength(
+		AccountNumberLengthUnitedStatesStart,
+		AccountNumberLengthUnitedStatesStop,
+	)
+	return randomNumberString(length)
 }
