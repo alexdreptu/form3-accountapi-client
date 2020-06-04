6e705392a4c06ea07f6545f37e9de979d5a652a2
Alexandru Dreptu <alexdreptu@gmail.com>
2020-06-04 14:51:37 +0300

    delete unused account related errors

--- account_errors.go
+++ account_errors.go
@@ -11,24 +11,6 @@ func (e *ResourceNotExistsError) Error() string {
 	return fmt.Sprintf("resource '%s' does not exist", e.Resource)
 }
 
-// InvalidVersionError is returned if the version arrgument that's passsed to DeleteAccount is not 0.
-type InvalidVersionError struct {
-	Version int
-}
-
-func (e *InvalidVersionError) Error() string {
-	return fmt.Sprintf("invalid version %d", e.Version)
-}
-
-// RecordNotExistsError is returned if trying to fetch and account that does not exist.
-type RecordNotExistsError struct {
-	Record string
-}
-
-func (e *RecordNotExistsError) Error() string {
-	return fmt.Sprintf("record '%s' does not exist", e.Record)
-}
-
 // DuplicateAccountError is returned if an account with that id already exists.
 type DuplicateAccountError struct {
 	ID string
